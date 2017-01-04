495e98758ca0a7f61d5ec81ccd4d7b32b437f4cf
Andrew Boryk <aboryk@mercymavericks.edu>
2017-01-04 13:41:56 -0500

    Initial Commit

---
+++ Example/Pods/Target Support Files/ABMediaView/ABMediaView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ABMediaView : NSObject
+@end
+@implementation PodsDummy_ABMediaView
+@end
---
+++ Example/Pods/Target Support Files/Pods-ABMediaView_Tests/Pods-ABMediaView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ABMediaView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ABMediaView_Tests
+@end
