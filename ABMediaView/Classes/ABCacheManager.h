b7aa37b48530fe59cccb26f4526674be2ccaf1e8
Andrew Boryk <aboryk@mercymavericks.edu>
2017-02-19 01:34:35 -0500

    Updated CacheType names and added loadImage and loadGIF

--- ABMediaView/Classes/ABCacheManager.h
+++ ABMediaView/Classes/ABCacheManager.h
@@ -17,19 +17,15 @@ typedef void (^GIFDataBlock)(UIImage *gif, NSString *key, NSError *error);
 
 /// Different types of caches
 typedef NS_ENUM(NSInteger, CacheType) {
-    ImageOriginal,
-    ImageThumbnail,
-    Video,
-    Audio,
-    GIF,
+    ImageCache,
+    VideoCache,
+    AudioCache,
+    GIFCache,
 };
 
 @interface ABCacheManager : NSObject {
-    /// Queue which holds requests for downloading full size images
-    NSCache *imageOriginalQueue;
-    
-    /// Queue which holds requests for downloading thumbnail images
-    NSCache *imageThumbnailQueue;
+    /// Queue which holds requests for downloading images
+    NSCache *imageQueue;
     
     /// Queue which holds requests for downloading videos
     NSCache *videoQueue;
@@ -40,11 +36,8 @@ typedef NS_ENUM(NSInteger, CacheType) {
     /// Queue which holds requests for downloading audio
     NSCache *audioQueue;
     
-    /// Cache which holds full size images
-    NSCache *imageOriginalCache;
-    
-    /// Cache which holds thumbnail size images
-    NSCache *imageThumbnailCache;
+    /// Cache which holds images
+    NSCache *imageCache;
     
     /// Cache which holds paths to videos on disk
     NSCache *videoCache;
@@ -56,11 +49,8 @@ typedef NS_ENUM(NSInteger, CacheType) {
     NSCache *audioCache;
 }
 
-/// Queue which holds requests for downloading full size images
-@property (retain, nonatomic) NSCache *imageOriginalQueue;
-
-/// Queue which holds requests for downloading thumbnail images
-@property (retain, nonatomic) NSCache *imageThumbnailQueue;
+/// Queue which holds requests for downloading images
+@property (retain, nonatomic) NSCache *imageQueue;
 
 /// Queue which holds requests for downloading videos
 @property (retain, nonatomic) NSCache *videoQueue;
@@ -71,11 +61,8 @@ typedef NS_ENUM(NSInteger, CacheType) {
 /// Queue which holds requests for downloading audio
 @property (retain, nonatomic) NSCache *audioQueue;
 
-/// Cache which holds full size images
-@property (retain, nonatomic) NSCache *imageOriginalCache;
-
-/// Cache which holds thumbnail size images
-@property (retain, nonatomic) NSCache *imageThumbnailCache;
+/// Cache which holds images
+@property (retain, nonatomic) NSCache *imageCache;
 
 /// Cache which holds paths to videos on disk
 @property (retain, nonatomic) NSCache *videoCache;
@@ -108,13 +95,13 @@ typedef NS_ENUM(NSInteger, CacheType) {
 - (void)removeFromQueue:(CacheType)type forKey:(NSString *)key;
 
 /// Load image and store in cache, or retrieve image from cache if already stored
-- (void)loadImage:(NSString *)image type:(CacheType)type completion:(ImageDataBlock)completionBlock;
+- (void)loadImage:(NSString *)urlString type:(CacheType)type completion:(ImageDataBlock)completionBlock;
 
 /// Load video and store in cache, or retrieve video from cache if already stored
-- (void)loadVideo:(NSString *)video type:(CacheType)type completion:(VideoDataBlock)completionBlock;
+- (void)loadVideo:(NSString *)urlString type:(CacheType)type completion:(VideoDataBlock)completionBlock;
 
 /// Load audio and store in cache, or retrieve audio from cache if already stored
-- (void)loadAudio:(NSString *)audio type:(CacheType)type completion:(AudioDataBlock)completionBlock;
+- (void)loadAudio:(NSString *)urlString type:(CacheType)type completion:(AudioDataBlock)completionBlock;
 
 /// Load GIF and store in cache, or retrieve gif from cache if already stored
 - (void)loadGIF:(NSString *)urlString type:(CacheType) type completion:(GIFDataBlock)completionBlock;
--- ABMediaView/Classes/ABCacheManager.m
+++ ABMediaView/Classes/ABCacheManager.m
@@ -11,14 +11,12 @@
 
 @implementation ABCacheManager
 
-@synthesize imageOriginalCache;
-@synthesize imageThumbnailCache;
+@synthesize imageCache;
 @synthesize videoCache;
 @synthesize audioCache;
 @synthesize gifCache;
 
-@synthesize imageOriginalQueue;
-@synthesize imageThumbnailQueue;
+@synthesize imageQueue;
 @synthesize videoQueue;
 @synthesize audioQueue;
 @synthesize gifQueue;
@@ -35,14 +33,12 @@
 - (id)init {
     if (self = [super init]) {
         // Initialize caches
-        imageOriginalCache = [[NSCache alloc] init];
-        imageThumbnailCache = [[NSCache alloc] init];
+        imageCache = [[NSCache alloc] init];
         videoCache = [[NSCache alloc] init];
         audioCache = [[NSCache alloc] init];
         gifCache = [[NSCache alloc] init];
         
-        imageOriginalQueue = [[NSCache alloc] init];
-        imageThumbnailQueue = [[NSCache alloc] init];
+        imageQueue = [[NSCache alloc] init];
         videoQueue = [[NSCache alloc] init];
         audioQueue = [[NSCache alloc] init];
         gifQueue = [[NSCache alloc] init];
@@ -53,19 +49,16 @@
 - (id) getCache: (CacheType) type objectForKey: (NSString *) key {
     if ([ABCommons notNull:key]) {
         switch (type) {
-            case ImageOriginal:
-                return [imageOriginalCache objectForKey:key];
+            case ImageCache:
+                return [imageCache objectForKey:key];
                 break;
-            case ImageThumbnail:
-                return [imageThumbnailCache objectForKey:key];
-                break;
-            case Video:
+            case VideoCache:
                 return [videoCache objectForKey:key];
                 break;
-            case Audio:
+            case AudioCache:
                 return [audioCache objectForKey:key];
                 break;
-            case GIF:
+            case GIFCache:
                 return [gifCache objectForKey:key];
                 break;
                 
@@ -80,19 +73,16 @@
 - (void) setCache: (CacheType) type object: (id) object forKey: (NSString *) key {
     if ([ABCommons notNull:object] && [ABCommons notNull:key]) {
         switch (type) {
-            case ImageOriginal:
-                [imageOriginalCache setObject:object forKey:key];
+            case ImageCache:
+                [imageCache setObject:object forKey:key];
                 break;
-            case ImageThumbnail:
-                [imageThumbnailCache setObject:object forKey:key];
-                break;
-            case Video:
+            case VideoCache:
                 [videoCache setObject:object forKey:key];
                 break;
-            case Audio:
+            case AudioCache:
                 [audioCache setObject:object forKey:key];
                 break;
-            case GIF:
+            case GIFCache:
                 [gifCache setObject:object forKey:key];
                 break;
             default:
@@ -106,19 +96,16 @@
 - (void) removeCache: (CacheType) type forKey: (NSString *) key {
     if ([ABCommons notNull:key]) {
         switch (type) {
-            case ImageOriginal:
-                [imageOriginalCache removeObjectForKey:key];
+            case ImageCache:
+                [imageCache removeObjectForKey:key];
                 break;
-            case ImageThumbnail:
-                [imageThumbnailCache removeObjectForKey:key];
-                break;
-            case Video:
+            case VideoCache:
                 [videoCache removeObjectForKey:key];
                 break;
-            case Audio:
+            case AudioCache:
                 [audioCache removeObjectForKey:key];
                 break;
-            case GIF:
+            case GIFCache:
                 [gifCache removeObjectForKey:key];
                 break;
             
@@ -134,19 +121,16 @@
 - (id) getQueue: (CacheType) type objectForKey: (NSString *) key {
     if ([ABCommons notNull:key]) {
         switch (type) {
-            case ImageOriginal:
-                return [imageOriginalQueue objectForKey:key];
+            case ImageCache:
+                return [imageQueue objectForKey:key];
                 break;
-            case ImageThumbnail:
-                return [imageThumbnailQueue objectForKey:key];
-                break;
-            case Video:
+            case VideoCache:
                 return [videoQueue objectForKey:key];
                 break;
-            case Audio:
+            case AudioCache:
                 return [audioQueue objectForKey:key];
                 break;
-            case GIF:
+            case GIFCache:
                 return [gifQueue objectForKey:key];
                 break;
                 
@@ -161,19 +145,16 @@
 - (void) addQueue: (CacheType) type object: (id) object forKey: (NSString *) key {
     if ([ABCommons notNull:object] && [ABCommons notNull:key]) {
         switch (type) {
-            case ImageOriginal:
-                [imageOriginalQueue setObject:object forKey:key];
+            case ImageCache:
+                [imageQueue setObject:object forKey:key];
                 break;
-            case ImageThumbnail:
-                [imageThumbnailQueue setObject:object forKey:key];
-                break;
-            case Video:
+            case VideoCache:
                 [videoQueue setObject:object forKey:key];
                 break;
-            case Audio:
+            case AudioCache:
                 [audioQueue setObject:object forKey:key];
                 break;
-            case GIF:
+            case GIFCache:
                 [gifQueue setObject:object forKey:key];
                 break;
                 
@@ -188,19 +169,16 @@
 - (void) removeFromQueue: (CacheType) type forKey: (NSString *) key {
     if ([ABCommons notNull:key]) {
         switch (type) {
-            case ImageOriginal:
-                [imageOriginalQueue removeObjectForKey:key];
+            case ImageCache:
+                [imageQueue removeObjectForKey:key];
                 break;
-            case ImageThumbnail:
-                [imageThumbnailQueue removeObjectForKey:key];
-                break;
-            case Video:
+            case VideoCache:
                 [videoQueue removeObjectForKey:key];
                 break;
-            case Audio:
+            case AudioCache:
                 [audioQueue removeObjectForKey:key];
                 break;
-            case GIF:
+            case GIFCache:
                 [gifQueue removeObjectForKey:key];
                 break;
                 
@@ -210,6 +188,248 @@
         }
     }
     
+}
+
+- (void)loadGIF:(NSString *)urlString type:(CacheType) type completion:(GIFDataBlock)completionBlock {
+    
+    if ([ABCommons notNull:urlString]) {
+        
+        NSURL *url = [NSURL URLWithString:urlString];
+        
+        if ([ABCommons notNull: [[ABCacheManager sharedManager] getCache:type objectForKey:urlString]]) {
+            
+            if(completionBlock) completionBlock([[ABCacheManager sharedManager] getCache:type objectForKey:urlString], urlString, nil);
+            
+        }
+        else {
+            if ([[ABCacheManager sharedManager] getQueue:type objectForKey:urlString] == nil) {
+                if ([ABCommons notNull:url]) {
+                    
+                    [[ABCacheManager sharedManager] addQueue:type object:urlString forKey:urlString];
+                    
+                    dispatch_queue_t downloadQueue = dispatch_queue_create("com.linute.processsmagequeue", NULL);
+                    dispatch_async(downloadQueue, ^{
+                        NSData *imageData = [NSData dataWithContentsOfURL:url];
+                        UIImage *image = [UIImage animatedImageWithAnimatedGIFData:imageData];
+                        
+                        if ([ABCommons notNull:urlString] && [ABCommons notNull:imageData]) {
+                            [[ABCacheManager sharedManager] setCache:type object:imageData forKey:urlString];
+                        }
+                        
+                        [[ABCacheManager sharedManager] removeFromQueue:type forKey:urlString];
+                        
+                        if(completionBlock) completionBlock(imageData, urlString, nil);
+                    });
+                }
+                else {
+                    if(completionBlock) completionBlock(nil, nil, nil);
+                }
+            }
+            else {
+                
+            }
+        }
+    }
+    else {
+        if(completionBlock) completionBlock(nil, nil, nil);
+    }
+    
+    
+    
+}
+
+- (void)loadImage:(NSString *)urlString type:(CacheType)type completion:(ImageDataBlock)completionBlock {
+    
+    if ([ABCommons notBlank:urlString]) {
+        
+        NSURL *url = [NSURL URLWithString:urlString];
+        
+        if ([ABCommons notNull: [[ABCacheManager sharedManager] getCache:type objectForKey:urlString]]) {
+            if(completionBlock) completionBlock([[ABCacheManager sharedManager] getCache:type objectForKey:urlString], urlString, nil);
+            
+        }
+        else {
+            if ([[ABCacheManager sharedManager] getQueue:type objectForKey:urlString] == nil) {
+                if ([ABCommons notNull:url]) {
+                    
+                    [[ABCacheManager sharedManager] addQueue:type object:urlString forKey:urlString];
+                    
+                    NSURLSessionTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                        if (data) {
+                            UIImage *image = [UIImage imageWithData:data];
+                            
+                            if (image) {
+                                dispatch_async(dispatch_get_main_queue(), ^{
+                                    if(completionBlock) completionBlock(image, urlString, nil);
+                                });
+                            }
+                        }
+                    }];
+                    
+//                    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+//                    manager.responseSerializer = [AFImageResponseSerializer serializer];
+//                    
+//                    manager.requestSerializer.cachePolicy = NSURLRequestReloadIgnoringLocalAndRemoteCacheData;
+//                    
+//                    //                    NSLog(@"URL %@", url);
+//                    
+//                    [manager GET:url.absoluteString parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+//                        
+//                        if (type == PostImageOriginal || type == ArticleImage) {
+//                            NSURL *notificationURL = [[CacheManager sharedManager] imageURL:image type:type];
+//                            if ([Utils notNull:notificationURL]) {
+//                                
+//                                NSNumber *progressNumber = [NSNumber numberWithFloat:downloadProgress.fractionCompleted];
+//                                NSString *progressImage = image;
+//                                
+//                                NSDictionary *progressDictionary = [[NSDictionary alloc] initWithObjectsAndKeys:progressNumber, @"progress", progressImage, @"image", nil];
+//                                
+//                                NSString *notificationString = [NSString stringWithFormat:@"Progress:%@", notificationURL.relativeString];
+//                                [[NSNotificationCenter defaultCenter] postNotificationName:notificationString object:progressDictionary];
+//                            }
+//                        }
+//                        
+//                        
+//                    } success:^(NSURLSessionTask *task, id responseObject) {
+//                        UIImage *responseImage = responseObject;
+//                        
+//                        if ([Utils notNull:image] && [Utils notNull:responseImage]) {
+//                            [Defaults cache:type setObject:responseImage forKey:image];
+//                        }
+//                        
+//                        [[CacheManager sharedManager] removeFromQueue:type forKey:image];
+//                        
+//                        NSURL *notificationURL = [[CacheManager sharedManager] imageURL:image type:type];
+//                        if ([Utils notNull:notificationURL]) {
+//                            [[NSNotificationCenter defaultCenter] postNotificationName:notificationURL.relativeString object:nil];
+//                        }
+//                        
+//                        if(completionBlock) completionBlock(responseImage, image, nil);
+//                        
+//                    } failure:^(NSURLSessionTask *operation, NSError *error) {
+//                        
+//                        NSLog(@"FAILURE %@", url);
+//                        [[CacheManager sharedManager] removeFromQueue:type forKey:image];
+//                        if(completionBlock) completionBlock(nil, nil, error);
+//                    }];
+                }
+                else {
+                    if(completionBlock) completionBlock(nil, nil, nil);
+                }
+            }
+            else {
+                
+            }
+        }
+    }
+    else {
+        if(completionBlock) completionBlock(nil, nil, nil);
+    }
+    
+}
+
+- (void)loadVideo:(NSString *)urlString type:(CacheType)type completion:(VideoDataBlock)completionBlock {
+    
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    if ([ABCommons notNull:urlString] && [ABCommons notNull:url]) {
+        if ([ABCommons notNull: [[ABCacheManager sharedManager] getCache:type objectForKey:urlString]]) {
+            if(completionBlock) completionBlock([[ABCacheManager sharedManager] getCache:type objectForKey:urlString], urlString, nil);
+        }
+        else {
+//            AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+//            
+//            NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//            
+//            NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:^(NSProgress * _Nonnull downloadProgress) {
+//                
+//                //        if(progressBlock) {
+//                //            progressBlock ([NSNumber numberWithDouble:downloadProgress.fractionCompleted]);
+//                //        }
+//                
+//            } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
+//                
+//                NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+//                return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+//                
+//            } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
+//                
+//                if(error) {
+//                    if(completionBlock) completionBlock(nil, nil, error);
+//                }
+//                else {
+//                    if ([Utils notNull:filePath.relativePath] && [Utils notNull:video]) {
+//                        [Defaults cache:type setObject:filePath.relativePath forKey:video];
+//                    }
+//                    if(completionBlock) completionBlock(filePath.relativePath, video, nil);
+//                }
+//                
+//            }];
+//            
+//            [downloadTask resume];
+        }
+    }
+    else {
+        if(completionBlock) completionBlock(nil, nil, nil);
+    }
+    
+}
+
++ (void) removeDocumentsVideos {
+    NSString *path = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/"];
+    NSArray *array = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:nil];
+    for (NSString *string in array) {
+        NSString *fullPath = [path stringByAppendingPathComponent:string];
+        
+        /// Make sure not to remove realm file
+        if (![fullPath containsString:@"realm"]) {
+            [[NSFileManager defaultManager] removeItemAtPath:fullPath error:nil];
+        }
+        
+    }
+}
+
+- (NSCache *) cacheForType: (CacheType) type {
+    switch (type) {
+        case ImageCache:
+            return imageCache;
+            break;
+        case VideoCache:
+            return videoCache;
+            break;
+        case AudioCache:
+            return audioCache;
+            break;
+        case GIFCache:
+            return gifCache;
+            break;
+            
+        default:
+            return nil;
+            break;
+    }
+}
+
+- (NSCache *) queueForType: (CacheType) type {
+    switch (type) {
+        case ImageCache:
+            return imageQueue;
+            break;
+        case VideoCache:
+            return videoQueue;
+            break;
+        case AudioCache:
+            return audioQueue;
+            break;
+        case GIFCache:
+            return gifQueue;
+            break;
+            
+        default:
+            return nil;
+            break;
+    }
 }
 
 @end
