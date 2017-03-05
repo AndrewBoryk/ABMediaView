147a648561e9ccef6ebb74fdf4e2ce28b80d56b1
Andrew Boryk <aboryk@mercymavericks.edu>
2017-03-04 20:02:58 -0500

    Updated test cases

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -36,7 +36,7 @@
 
 - (void)testImageLoading {
     [ABCacheManager loadImage:@"http://camendesign.com/code/video_for_everybody/poster.jpg" completion:^(UIImage *image, NSString *key, NSError *error) {
-        if ([ABCommons notNull:image]) {
+        if ([ABCommons isNull:image]) {
             XCTAssertNotNil(image, "image should not be nil");
         }
     }];
@@ -44,7 +44,7 @@
 
 - (void)testVideoLoading {
     [ABCacheManager loadVideo:@"http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" completion:^(NSURL *videoPath, NSString *key, NSError *error) {
-        if ([ABCommons notNull:videoPath]) {
+        if ([ABCommons isNull:videoPath]) {
             XCTAssertNotNil(videoPath, "videoPath should not be nil");
         }
     }];
@@ -52,7 +52,7 @@
 
 - (void)testGIFLoading {
     [ABCacheManager loadGIF:@"http://static1.squarespace.com/static/552a5cc4e4b059a56a050501/565f6b57e4b0d9b44ab87107/566024f5e4b0354e5b79dd24/1449141991793/NYCGifathon12.gif" completion:^(UIImage *gif, NSString *key, NSError *error) {
-        if ([ABCommons notNull:gif]) {
+        if ([ABCommons isNull:gif]) {
             XCTAssertNotNil(gif, "gif should not be nil");
         }
     }];
@@ -60,7 +60,7 @@
 
 - (void)testAudioLoading {
     [ABCacheManager loadAudio:@"https://a.tumblr.com/tumblr_ojs6z4VJp31u5escjo1.mp3" completion:^(NSURL *audioPath, NSString *key, NSError *error) {
-        if ([ABCommons notNull:audioPath]) {
+        if ([ABCommons isNull:audioPath]) {
             XCTAssertNotNil(audioPath, "audioPath should not be nil");
         }
     }];
